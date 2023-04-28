8c937514f0bbe27081729e57daef44ec6ba17467
Elena Bychenkova <elena.bychenkova@gmail.com>
2023-04-27 20:24:12 -0700

    feat: Create AJAX calls for two functions

--- static/ajax-review.js
+++ static/ajax-review.js
@@ -2,6 +2,12 @@
 
 function showFortune() {
   // TODO: get the fortune and show it in the fortune-text div
+  fetch('/fortune')
+    .then((response) => response.text())
+    .then((fortune) => {
+      const fortuneElement = document.querySelector('#fortune-text');
+      fortuneElement.innerHTML = fortune;
+    });
 }
 
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
@@ -13,6 +19,12 @@ function showWeather(evt) {
   const zipcode = document.querySelector('#zipcode-field').value;
   const url = `/weather?zipcode=${zipcode}`;
   // TODO: request weather with that URL and show the forecast in #weather-info
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsonData) => {
+      const weatherElement = document.querySelector('#weather-info');
+      weatherElement.innerText = jsonData.forecast;
+    });
 }
 
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
